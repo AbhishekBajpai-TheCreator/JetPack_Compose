3172613d2f4f19c711963605247f305b9832543b
Abhishek Bajpai <abhishekbajpai0449@gmail.com>
2023-12-13 23:00:12 +0530

    Commit By Abhi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -70,4 +70,19 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:3.14.9")
+    //Coil
+    implementation("io.coil-kt:coil:1.4.0")
+    //Dagger Hilt
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    implementation ("com.google.dagger:hilt-android:2.44.1")
+    //View Model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    //LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
 }
